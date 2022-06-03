912349ac44769f72b4215232139a88e6e0d100b9
Ola Ishola <olaishola@hotmail.co.uk>
2022-06-03 11:19:35 +0100

    Refactor the Greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchGreetingsFromApi } from '../redux/greetings/greetingReducer';
 
 const HelloWorld = () => {
   const greeting = useSelector((state) => state.greetings);
-  const { messagee } = greeting;
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,7 +14,7 @@ const HelloWorld = () => {
   return (
     <div className="greeting-container">
       <h1>Hello! This is greetings page</h1>
-      <h2 className="text">{messagee}</h2>
+      <h2 className="text">{greeting.messagee}</h2>
       <button type="button" onClick={() => dispatch(fetchGreetingsFromApi())} className="btn">Click for next greeting</button>
       <br />
       <Link to="/" className="link-to">Home page</Link>
@@ -24,9 +22,4 @@ const HelloWorld = () => {
   );
 };
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.shape({
-    messagee: PropTypes.string.isRequired,
-  }).isRequired,
-};
 export default HelloWorld;
